2632855a4e0a8ea1f5f819e0fa661960f186564b
GabrielSam25 <samtestesoparaisso@gmail.com>
2025-09-25 23:37:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import discord
 import os
 from discord.ext import commands
 
-client = commands.Bot(command_prefix=':', self_bot=True, help_command=None)
+# Configurando os intents
+intents = discord.Intents.default()
+intents.message_content = True  # necessário para ler mensagens no Discord
+
+client = commands.Bot(command_prefix=':', self_bot=True, help_command=None, intents=intents)
 
 @client.event
 async def on_ready():
-  await client.change_presence(status=discord.Status.online)
-  os.system('clear')
-  print(f'Logged in as {client.user} (ID: {client.user.id})')
+    await client.change_presence(status=discord.Status.online)
+    os.system('clear')
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
 
 client.run(os.getenv("TOKEN"), bot=False)
